04bc45b4053e08ac8064633cb2962581a31c6f42
ForgottenUmbrella <regretfulumbrella@gmail.com>
2016-12-17 19:28:10 +1100

    Restarted the project; added parsers

--- booru_wallpaper.py
+++ booru_wallpaper.py
@@ -7,7 +7,8 @@ tags. The image is downloaded and optionally edited before being set as
 the Windows desktop wallpaper. After a specified amount of time (via a
 timer service), the process starts over.
 
-TODO:
+##TODO:
+add everything again...
 Method or function??
 Log in to Danbooru, bypass limit
 Platform specific wallpaper settings (e.g. positioning)
@@ -32,192 +33,157 @@ Implement `show c' `show w'
 regretfulumbrella@gmail.com
 """
 
-__author__ = "Leo Pham"
+__author__ = 'Leo Pham'
 __credits__ = []
-__date__ = "2016-11-20"
-__version__ = "0.1"
-__status__ = "Prototype"
+__date__ = '2016-11-20'
+__version__ = '0.1'
+__status__ = 'Prototype'
 
-import urllib.parse
-import urllib.request
 import os
 import sys
-import sched
-import time
-import configParser
+import getpass
+import argparse
+import urllib.parse
+import urllib.request
+import configparser
 import json
 # Platform dependent
-if sys.platform == "win32": import ctypes
-elif sys.platform == "darwin": import subprocess
+if sys.platform == 'win32': import ctypes
+elif sys.platform == 'darwin': import subprocess
+##disabled because py3.6 can't
+# import PIL.Image
+# import PIL.ImageEnhance  # .Color, .Brightness
+# import PIL.ImageFilter  # .filter
 
-import PIL.Image
-import PIL.ImageEnhance  # .Color, .Brightness
-import PIL.ImageFilter  # .filter
-
-
-def gram_join(string, splitter=" ", sep=", ", end=" and "):
-    """Return a grammatically correct string from a string list."""
-    if len(list_) == 1:
-        return list_[0]
-    string_list = string.split(splitter)
-    all_but_last = sep.join(string_list[:-1])
-    last = string_list[-1]
-    return end.join(all_but_last, last)
-
-
-def set_wallpaper(wallpaper):
-    """Set the desktop wallpaper."""
-    if sys.platform == "win32":
-        SPI_SETDESKWALLPAPER = 20
-        ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0,
-            wallpaper.path, 3)
-    elif sys.platform == "linux":
-        os.system("/usr/bin/gsettings "
-            f"set org.gnome.desktop.background picture-uri {wallpaper.path}")
-    elif sys.platform == "darwin":
-        SCRIPT = "tell application 'Finder' to set desktop picture to POSIX " \
-            f"file '{wallpaper.path}'"
-        subprocess.Popen(SCRIPT, shell=True)
-    else:
-        raise NotImplemented
-
-s = sched.scheduler()
-s.enterabs(get_next_time, 1, set_wallpaper, DanbooruWallpaper(search))
-def get_next_time(duration):
-    return time.time() + duration
-    # Set service
-    # Await time
-    # Delete image
-    # Get new wallpaper
-    # Repeat...
-    pass
-
-
-def edit_wallpaper(wallpaper, blur, grey, dim):
-    return DanbooruWallpaper(image.path)
-
-
-class DanbooruWallpaper:
-    """An online image from Danbooru with metadata."""
-
-    def __init__(self, search):
-        """Retrieve an image and set metadata.
-
-        Note to self that search may be an id.
-        """
-        # Might someday be changeable, therefore not constant
-        image_board = "https://danbooru.donmai.us"
-        posts_list = f"{image_board}/posts.json"
-        # Required to handle spaces and other such dangers safely
-        query = urllib.parse.urlencode({
-            'limit': 1,
-            'tags': search,
-            'random': "true"
-        })
-        print("Retrieving image...")
-        request = urllib.request.urlopen(f"{posts_list}?{query}")
-        data = json.loads(request.read().decode('ascii'))[0]
-        # Useful data
-        self.id = int(data['id'])
-        self.height = int(data['image_height'])
-        self.width = int(data['image_width'])
-        self.characters = data['tag_string_character'].split()
-        self.copyrights = data['tag_string_copyright'].split()
-        self.artists = data['tag_string_artist']
-        self.tags = data['tag_string_general']
-        self.url = f"{image_board}{data['file_url']}"
-        self.file_ext = data['file_ext']
-        self.title = f"{gram_join(self.characters)} from "
-            f"{gram_join(self.copyrights)}, drawn by {gram_join(self.artists)}"
-        urllib.request.urlretrieve(self.url, file_name)
-        self.path = os.path.join(os.getcwd(), file_name)
-
-    def download(self, file_name=f"{self.title}.{self.file_ext}"):
-        """Download an image, set path and return it.
-        
-        Not to be used for saving edited images, as the Pillow module
-        will handle that. Available as a method since downloading may fail.
-        """
-        print("Downloading image...")
-        urllib.request.urlretrieve(self.url, file_name)
-        self.path = os.path.join(os.getcwd(), file_name)
-        return self.path
-
-    def edit(self, blur, grey, dim):
-        """Edit the image for wallpaper suitability and save it."""
-        
-        self.path
-        self.blur
-        self.grey
-        self.dim
-        self.duration # ?
-
-    def view():
-
-
-def view_image(image):
-    """Open the image viewer to view the full image, unedited"""
-    pass
-
-
-def set_wallpaper(image):
-    """Get, save, edit and Set an image as the Windows desktop wallpaper"""
-    image_url = get_image(search)
-    image_path = save_image(image_url)
-    wallpaper = edit_image(image_path)
-    set_wallpaper(wallpaper)
-    set_timer(duration)
-    pass
-
-
-def get_credits(image_url):
-    """"""
-    pass
-
-
-# Somewhere, a service is checking for the time and calling set_wallpaper()
-
-print("""
-    Booru Wallpaper  Copyright (C) 2016  Leo Pham
+DISCLAIMER = f'''
+    Booru Wallpaper  Copyright (C) {__date__[:3]}  {__author__}
     This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
     This is free software, and you are welcome to redistribute it
     under certain conditions; type `show c' for details.
-    """)
+'''
+CONDITIONS = '''
+    5. Conveying Modified Source Versions.
 
-while True:
-    print("Danbooru Wallpaper v0.1 - Forgotten Umbrella")
-    print("Set your wallpaper to a random image from Danbooru, "
-          "based on the provided tags.")
-    print()
-    print("Actions")
-    print("\t1. Set tags\n"
-          "\t2. View full image\n"
-          "\t3. View credits\n"
-          "\t4. Change to next wallpaper\n"
-          "\t5. Customise wallpaper\n"
-          "\t6. Set rotation interval\n")
-    
-    while True:
-        choice = input("Input your choice (number) >>> ")
-        try:
-            if int(choice) not in range(1,7):
-            # Inclusive min, exclusive max
-                raise ValueError
-        except ValueError:
-            print("Input was not a valid number.")
-        else:
-            choice = int(choice)
-            break
-    print()
+    You may convey a work based on the Program, or the modifications to produce
+    it from the Program, in the form of source code under the terms of
+    section 4, provided that you also meet all of these conditions:
 
-    if choice == 1:
-        while True:
-            query = input("Input tags to search on Danbooru (maximum of two) >>> ")
-            try:
-                image_url = get_image(query)
-            except urllib.error.HTTPError:
-                print("Too many tags.")
-            else:
-                break
-        set_timer(duration)
+    a) The work must carry prominent notices stating that you modified it, and
+       giving a relevant date.
+    b) The work must carry prominent notices stating that it is released under
+       this License and any conditions added under section 7. This requirement
+       modifies the requirement in section 4 to “keep intact all notices”.
+    c) You must license the entire work, as a whole, under this License to
+       anyone who comes into possession of a copy. This License will therefore
+       apply, along with any applicable section 7 additional terms, to the whole
+       of the work, and all its parts, regardless of how they are packaged.
+       This License gives no permission to license the work in any other way,
+       but it does not invalidate such permission if you have separately
+       received it.
+    d) If the work has interactive user interfaces, each must display
+       Appropriate Legal Notices; however, if the Program has interactive
+       interfaces that do not display Appropriate Legal Notices, your work need
+       not make them do so.
+'''
+WARRANTY = '''
+    15. Disclaimer of Warranty.
 
+    THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE
+    LAW. EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR
+    OTHER PARTIES PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND,
+    EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+    ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.
+    SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY
+    SERVICING, REPAIR OR CORRECTION.
+'''
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Set the wallpaper to a '
+        'random image from a booru-based imageboard every now and then.')
+    subparsers = parser.add_subparsers()
+    # Set
+    parser_set = subparsers.add_parser('set', help='set the tags, imageboard '
+        'and duration of images')
+    parser_set.add_argument('-t', '--tags', nargs='+', help='a space-delimited '
+        'list of tags to search; only two tags allowed for unregistered '
+        'Danbooru users')
+    parser_set.add_argument('-s', '--source', choices=['danbooru', 'safebooru',
+        'moebooru', 'gelbooru'], help='an imageboard to source images from')
+        ##default='danbooru' if first-time else confparser.source
+    parser_set.add_argument('-d', '--duration', type=int, help='an integer '
+        'for how many hours an image will be used as the wallpaper before it '
+        'is changed')
+        ##default=same idea as above
+    # Edit
+    PERCENTAGE = list(range(100)) + [100]
+    parser_edit = subparsers.add_parser('edit', help='edit the wallpaper\'s '
+        'appearance - the amount of blurriness, greyness and dimness to apply')
+    parser_edit.add_argument('-b', '--blur', type=int, choices=PERCENTAGE,
+        help='an integer for the wallpaper\'s blurriness')
+        ##default=same
+    parser_edit.add_argument('-g', '--grey', '--gray', type=int,
+        choices=PERCENTAGE, help='an integer, from 0 to 100 inclusive, for the '
+        'wallpaper\'s greyness (yes that\'s a word, probably)')
+        ##default=same
+    parser_edit.add_argument('-d', '--dim', type=int, choices=PERCENTAGE,
+        help='an integer for the wallpaper\'s dimness')
+        ##default=same
+    # View
+    parser_view = subparsers.add_parser('view', help='view the image, its '
+        'tags (artist, characters, copyright, other), webpage or source')
+    parser_view.add_argument('view_choice', choices=['image', 'tags', 'page',
+        'source'], help='what to view - will be opened in the relevant apps')
+    # Login
+    parser_login = subparsers.add_parser('login', help='log in to the selected '
+        'imageboard, to bypass its limits on the number of tags and '
+        'censorship, you dead animal-f*cking pedophile')
+    parser_login.add_argument('user', help='the account name to log in to; '
+        'a secure password prompt will appear after inputting this')
+    # Everything else
+    parser.add_argument('-n', '--next', action='store_true', help='change the '
+        'wallpaper')
+    parser.add_argument('-s', '--stop', action='store_true', help='begone '
+        'unholy spirit, begone! (kill the daemon service running in the '
+        'background)')
+    parser.add_argument('-v', '--version', action='version',
+        version='%(prog)s '+__version__, help='display the version')
+    ##will use fstrings when py3.6 available
+
+    if len(sys.argv) == 1:
+        argv = interactive_main()
+        
+    args = vars(parser.parse_args(argv))
+
+    ##os.system('cron or schtask command to call booru_wallpaper.py -n
+    ##every now and then if not already started on first run')
+
+
+def interactive_main():
+    ##Convert input into argv
+    print(DISCLAIMER)
+    while (True):
+        print('Actions')
+        print('\t1. Set tags\n'
+                'set source'
+                'blur'
+                'grey'
+                'dim'
+                'login'
+                'return to reality'
+              '\t2. View full image\n'
+              '\t3. View credits\n'
+              '\t4. Change to next wallpaper\n'
+              '\t5. Customise wallpaper\n'
+              '\t6. Set rotation interval\n')
+        ##don't use numbers, so can prompt "show c" "show w"
+        action = input('Input action: ')
+        if action == 'show c': print(CONDITIONS)
+        elif action == 'show w': print(WARRANTY)
+        # elif action == ...
+        else: print('no.')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
